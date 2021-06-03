dbd0e473be070f848271b325243fb606a42a2ff0
Harsha Varshney <66527713+harsha1223@users.noreply.github.com>
2021-06-04 00:13:24 +0530

    Add files via upload

---
+++ Graph/InsertionOperation/AddNode.py
@@ -0,0 +1,25 @@
+def add_node(v):
+    global node_count
+    if (v in nodes):
+        print("The node already present in the graph")
+    else:
+        node_count = node_count + 1
+        nodes.append(v)
+        for n in graph:
+            n.append(0)
+        temp = []
+        for i in range (node_count):
+            temp.append(0)
+        graph.append(temp)
+
+node_count = 0
+nodes = [] 
+graph = []
+# print("Before adding nodes")
+# print(nodes)
+# print(graph)
+# add_node("A")
+# add_node("B")
+# print("After adding nodes")
+# print(nodes)
+# print(graph)
